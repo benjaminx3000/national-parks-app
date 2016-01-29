4968ffa6d5d72a90e4a686dae50aaf3fe6e48968
Ben Liesch <ben.liesch@7summitsagency.com>
2016-01-29 15:46:43 -0600

    added custom search module

---
+++ app/search/search.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {eventManager} from '../core/event-manager';
+
+export default class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        // Set initial sate
+        this.state = {
+            searchQuery: '',
+            isActive: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleFocus = this.handleFocus.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+    }
+
+    componentDidMount() {
+        this.state.searchQuery = this.props.searchQuery;
+        this.state.isActive = this.props.isActive;
+    }
+
+    handleChange(e) {
+        this.setState({searchQuery: e.target.value})
+        eventManager.publish('searchUpdate', e.target.value);
+    }
+
+    handleFocus() {
+        this.setState({isActive: true});
+        //update the filter value
+    }
+
+    handleBlur() {
+        this.setState({isActive: false});
+        //update the route
+    }
+
+    render() {
+        return (
+            <div className="parks-search">
+                <input
+                    type="search"
+                    className={`parks-search__input ${(this.state.isActive) ? 'is-active': 'not-active'}`}
+                    value={this.props.searchQuery}
+                    onFocus={this.handleFocus}
+                    onBlur={this.handleBlur}
+                    onChange={this.handleChange}
+                />
+            </div>
+        );
+    }
+}
